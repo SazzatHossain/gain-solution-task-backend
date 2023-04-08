8e03fd83446ece85d19e0388acbe5954ddeeebae
SazzatHossain <sazzathossaindipto@gmail.com>
2023-04-08 22:40:38 +0600

    WIP: modify Event index funtion

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
-
 Rails.application.routes.draw do
 
   scope '/api' do
-    namespace :v1, defaults: { format: :json} do
-       resources :events do
-          resources :rsvps
-        end
+    namespace :v1, defaults: { format: :json } do
+      resources :events do
+        resources :rsvps
+      end
       resources :home, :path => '/', only: [:index]
       resources :users
       get '/user-data', to: 'users#show'
+      patch '/user-data-update', to: 'users#update'
       post '/sign-in', to: 'sessions#sign_in'
     end
   end
