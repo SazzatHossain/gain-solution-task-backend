d0f1586c58abc864f2d2fcfe4f3c0ebaff5effdf
Sazzat Hossain <sazzathossaindipto@gmail.com>
2023-04-09 14:12:54 +0600

    RSVP model and controller created

--- app/controllers/v1/rsvps_controller.rb
+++ app/controllers/v1/rsvps_controller.rb
@@ -1,5 +1,5 @@
 class V1::RsvpsController < V1::BaseController
-  before_action :authenticate_user, only: [:create, :update]
+  before_action :authenticate_user, only: [:create, :update, :show_rsvp]
 
  def index
     @rsvps = Rsvp.all
@@ -11,6 +11,23 @@ class V1::RsvpsController < V1::BaseController
     @rsvp = event.rsvps.find(params[:id])
   end
 
+def show_rsvp
+      user = User.find_by(id: @user.id)
+      event = Event.find(params[:event_id])
+      if event.rsvps.where(user_id: @user.id).exists?
+              result = {
+                message: "true",
+                status: "200"
+              }
+         render json: result, status: 409 and return
+      else
+              result = {
+                message: "false",
+                status: "409"
+              }
+         render json: result, status: 409 and return
+      end
+     end
   def create
       user = User.find_by(id: @user.id)
       event = Event.find(params[:event_id])
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,14 @@ Rails.application.routes.draw do
     namespace :v1, defaults: { format: :json } do
       resources :events do
         resources :rsvps
-      end
+        get '/show-rsvp', to: "rsvps#show_rsvp"
+     end
       resources :home, :path => '/', only: [:index]
       resources :users
       get '/user-data', to: 'users#show'
       get '/my-events-list', to: 'events#my_events_list'
       patch '/user-data-update', to: 'users#update'
       post '/sign-in', to: 'sessions#sign_in'
-    end
+     end
   end
 end
